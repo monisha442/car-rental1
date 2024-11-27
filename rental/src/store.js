492b0bf3f118d90d28db2378a85aa5af15a89edf
monisha442 <monishav2573@gmail.com>
2024-11-27 19:48:45 +0530

    Delete rental directory

--- rental/src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer/rootReducer';
-
-function configureStore(state = { progress: 1, location: '', Car:''}) {
-    return createStore(rootReducer,state);
-}
-
-export default configureStore;
